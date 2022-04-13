76e80c28388f4fe53f4b5d14b62a9cc4f9c962ca
devHudi <devhudi@gmail.com>
2022-04-13 17:14:02 +0900

    fix: IsUrl 데코레이터 메세지 옵션 이슈 해결

--- src/restaurants/images/dto/image.dto.ts
+++ src/restaurants/images/dto/image.dto.ts
@@ -9,11 +9,13 @@ export class RestaurantImageDTO {
   restaurantId: RestaurantImage["restaurantId"]
 
   @IsString({ message: "올바른 이미지 URL을 입력해주세요." })
-  @IsUrl({
-    require_protocol: true,
-    require_valid_protocol: true,
-    message: "올바르지 않은 URL 형태입니다.",
-  })
+  @IsUrl(
+    {
+      require_protocol: true,
+      require_valid_protocol: true,
+    },
+    { message: "올바르지 않은 URL 형태입니다." },
+  )
   imageUrl: RestaurantImage["imageUrl"]
 
   @IsString({ message: "올바른 캡션을 입력해주세요." })
